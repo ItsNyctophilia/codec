9490a4fcac4b91481605e40fc2d85da31e05bbcd
James Viner Esq <jrviner2000@gmail.com>
2022-11-28 19:59:39 -0500

    Add framework for parsing decode output text

--- encode.c
+++ encode.c
@@ -16,6 +16,9 @@ static struct {
 	bool little_endian;
 } options = { false };
 
+int parse_packet_contents(FILE * fo);
+int skip_to_next_packet(FILE * fo);
+
 int main(int argc, char *argv[])
 {
 	int opt;
@@ -52,5 +55,80 @@ int main(int argc, char *argv[])
 	// TODO: parse_packet_contents(fo);
 
 	fclose(fo);
+	return (0);
+}
+
+int parse_packet_contents(FILE * fo)
+{
+	char *line_buf = NULL;
+	size_t buf_size = 0;
+
+	getline(&line_buf, &buf_size, fo);
+	char *word = strtok(line_buf, ":");
+	//printf("%s <--- string\n", word);
+	if (strcmp(word, "Version") != 0) {
+		skip_to_next_packet(fo);
+	}
+	word = strtok(NULL, " \n");
+	// Set Version field in packet  
+
+	getline(&line_buf, &buf_size, fo);
+	strtok(line_buf, ":");
+	if (strcmp(word, "Sequence") != 0) {
+		skip_to_next_packet(fo);
+	}
+	// Set sequence num in packet
+
+	getline(&line_buf, &buf_size, fo);
+	strtok(line_buf, ":");
+	if (strcmp(word, "From") != 0) {
+		skip_to_next_packet(fo);
+	}
+	// Set From num in packet
+
+	getline(&line_buf, &buf_size, fo);
+	strtok(line_buf, ":");
+	if (strcmp(word, "To") != 0) {
+		skip_to_next_packet(fo);
+	}
+	// Set To num in packet
+
+	getline(&line_buf, &buf_size, fo);
+	strtok(line_buf, ":");
+	if (strcmp(word, "Message") == 0) {
+		// Parse Message packet
+	} else if (strcmp(word, "Command") == 0) {
+		// Parse Command packet
+	} else if (strcmp(word, "Max Hit Points") == 0) {
+		// Parse Status packet
+	} else if (strcmp(word, "Latitude") == 0) {
+		// Parse GPS packet
+	} else {
+		skip_to_next_packet(fo);
+	}
+
+	if (line_buf) {
+		free(line_buf);
+	}
+	return (1);
+}
+
+int skip_to_next_packet(FILE * fo)
+{
+	char *line_buf = NULL;
+	size_t buf_size = 0;
+
+	for (;;) {
+		unsigned int offset = ftell(fo);
+		if ((getline(&line_buf, &buf_size, fo) != -1)) {
+			if (!strcmp(strtok(line_buf, " :\n"), "Version")) {
+				fseek(fo, offset, SEEK_SET);
+				return (1);
+			}
+			continue;
+		}
+		break;
+	}
+
 	return (0);
 }
